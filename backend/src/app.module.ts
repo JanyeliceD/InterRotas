450edb7e7fc15ff315bb4f83936bee80a8c098cf
JanyeliceD <janyelice.d@escolar.ifrn.edu.br>
2026-05-01 19:48:35 -0300

    Integração de rotas com paradas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventoModule } from './evento/evento.module';
 import { ParadasModule } from './paradas/paradas.module';
+import { RotasModule } from './rotas/rotas.module';
 
 @Module({
-  imports: [EventoModule, ParadasModule],
+  imports: [EventoModule, ParadasModule, RotasModule ],
   controllers: [AppController],
   providers: [AppService],
 })
